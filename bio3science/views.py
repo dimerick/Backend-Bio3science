0bd54aa1db1cfff7fa0ef9cba3aded67ed132017
Erick Saenz <ericksaenz@outlook.com>
2020-09-02 21:49:01 -0500

    fixing model

--- bio3science/views.py
+++ bio3science/views.py
@@ -5,7 +5,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status, permissions
-from django.contrib.auth.models import User
+from .models import CustomUser
 from .serializers import CustomUserSerializer
 from django.http import Http404
 
@@ -42,7 +42,7 @@ class AccountDetail(APIView):
 
     def get_object(self, pk):
         try:
-            return User.objects.get(id=pk)
+            return CustomUser.objects.get(id=pk)
         except User.DoesNotExist:
             raise Http404
 
